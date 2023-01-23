e9e2976f7fed73070f5642aed7f7a6679e51e7f2
boobogo <80722701+boobogo@users.noreply.github.com>
2023-01-23 14:09:13 +0100

    Add files via upload

---
+++ filter.py
@@ -0,0 +1,14 @@
+import itertools
+
+seq = "123456"
+com_seq = itertools.combinations(seq, 6)
+count = 57
+for c in com_seq:  
+    print('case {}:  result = cross && f{} && f{} && f{} && f{} && f{} && f{};	break;'.format(count,c[0],c[1],c[2],c[3],c[4],c[5]))
+    count+=1
+	
+print('total combination: ',count-1)
+
+for i in range(1,7):
+	print('bool f{} = macdFilter{}[0] > signalFilter{}[0] && signalFilter{}[0] < 0.0;'.format(i,i,i,i))
+
